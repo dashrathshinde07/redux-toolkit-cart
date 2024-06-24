747309fd470df4ee3777adcc034f1b7c23b8b64f
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-24 15:38:31 +0530

    Set up routing with React Router: Added BrowserRouter, Routes, and Route components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
 const App = () => {
-  return <>Shopping Cart</>;
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
